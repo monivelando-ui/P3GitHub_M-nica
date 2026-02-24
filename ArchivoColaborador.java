d30c70b4d7580e6f0179c543920112205193765f
monivelando-ui <monivelando@icloud.com>
2026-02-24 11:36:19 +0100

    Update ArchivoColaborador.java
    pull request del colaborador

--- ArchivoColaborador.java
+++ ArchivoColaborador.java
@@ -1,5 +1,5 @@
 public class ArchivoColaborador {
     public static void main(String[] args) {
-        System.out.println("Archivo creado por el otro colaborador");
+        System.out.println("Archivo creado por el otro colaborador 2");
     }
 }
